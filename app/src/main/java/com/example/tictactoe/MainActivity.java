57858fc533334466d583ab53111a180a8779e680
carmeli <carmeli.omri@gmail.com>
2023-12-26 19:06:59 +0200

    function onNewGame created

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    String turn;
+    String [] [] board;
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void onNewGame() {
+        board = new String[3][3];
+        for (int row=0; row<3; row++)
+            for (int col=0; col<3; col++)
+                board[row][col]= new String();
+        turn = "x";
+        count = 0;
+    }
+
+    public void onButtonClick(View view) {
+    }
+}
